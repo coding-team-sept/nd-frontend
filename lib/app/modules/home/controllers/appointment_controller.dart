1f9b0e71246ef021d5123e91f27b01de4cc8eba8
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-08-29 15:46:39 +1000

    Updated AppointmentTile widget and wiring up with callback, Updated model with id and consultation location

--- lib/app/modules/home/controllers/appointment_controller.dart
+++ lib/app/modules/home/controllers/appointment_controller.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../model/appointment_model.dart';
 
 class AppointmentController extends GetxController {
+  // TODO: Replace with actual data
   RxList<AppointmentModel> appointmentData = <AppointmentModel>[
-    AppointmentModel(name: "Ravel Tanjaya", time: "23 August 2022"),
-    AppointmentModel(name: "Xiao Ming", time: "23 August 2022"),
-    AppointmentModel(name: "John Doe", time: "23 August 2022"),
+    AppointmentModel(
+        id: "ravel",
+        name: "Ravel Tanjaya",
+        time: "23 August 2022",
+        isOnline: true),
+    AppointmentModel(
+        id: "ming", name: "Xiao Ming", time: "23 August 2022", isOnline: false),
+    AppointmentModel(
+        id: "doe", name: "John Doe", time: "23 August 2022", isOnline: true),
   ].obs;
 
   @override
@@ -26,5 +34,12 @@ class AppointmentController extends GetxController {
 
   void newAppointment() {
     // Navigate to new appointment screen
+  }
+
+  void showAppointmentDetail(String id) {
+    Get.dialog(AlertDialog(
+      title: const Text("Not implemented"),
+      content: Text(id),
+    ));
   }
 }
