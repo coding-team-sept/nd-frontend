0a45efc3f520ade7c5a884a67605532bb3226dab
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2022-08-29 17:40:04 +1000

    Updated Dashboard controller DI to use binding

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,12 +1,12 @@
 import 'package:get/get.dart';
+import 'package:nd/app/modules/home/controllers/dashboard_controller.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(
-      () => HomeController(),
-    );
+    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<DashboardController>(() => DashboardController());
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:nd/app/modules/home/controllers/dashboard_controller.dart';
 import 'package:nd/app/modules/home/views/dashboard_view.dart';
 
 import '../controllers/home_controller.dart';
@@ -10,7 +9,6 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    Get.put(DashboardController());
     return DefaultTabController(
       length: 3,
       child: Scaffold(
