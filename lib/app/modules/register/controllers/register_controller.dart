5fd24a0a9909c88c3375f095b7823ff69788264d
Wenlao Peng <s3690691@student.rmit.edu.au>
2022-09-03 14:52:16 +1000

    add show password function and update format confirm function

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegisterController extends GetxController {
+  final fullName = "".obs;
+  final email = "".obs;
+  final password = "".obs;
+  final confirmPassword = "".obs;
 
-  var fullName = "".obs;
-  var email = "".obs;
-  var password = "".obs;
-  var confirmPassword ="".obs;
+  final fullNameError = "".obs;
+  final emailError = "".obs;
+  final passwordError = "".obs;
+  final confirmPasswordError = "".obs;
 
-  var fullNameError = "".obs;
-  var emailError = "".obs;
-  var passwordError = "".obs;
-  var confirmPasswordError = "".obs;
+  final showPassword = true.obs;
+  final showConfirmPassword = true.obs;
 
+  void toggleShowPassword() {
+    showPassword.value = !showPassword.value;
+  }
 
+  void toggleShowConfirmPassword() {
+    showConfirmPassword.value = !showConfirmPassword.value;
+  }
 
   @override
   void onInit() {
@@ -30,53 +38,64 @@ class RegisterController extends GetxController {
     super.onClose();
   }
 
-  void onNameChange(String newName) =>fullName.value = newName;
+  void onNameChange(String newName) => fullName.value = newName;
 
+  void onPasswordChange(String newName) => password.value = newName;
 
-  void onPasswordChange(String newName) =>password.value = newName;
+  void onConfirmPasswordChange(String newName) =>
+      confirmPassword.value = newName;
 
+  void onEmailChange(String newName) => email.value = newName;
 
-  void onConfirmPasswordChange(String newName) =>confirmPassword.value = newName;
+  void signUp() {
+    fullNameError.value = "";
+    emailError.value = "";
+    passwordError.value = "";
+    confirmPasswordError.value = "";
 
-  void onEmailChange(String newName) =>email.value = newName;
-
-
-  void signUp(){
-
-    bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email.value);
     bool success = true;
 
-    if (fullName.value.length<3){
+    if (fullName.value.length < 3) {
       fullNameError.value = "Please input at least 3 characters";
       success = false;
     }
-    if (password.value != confirmPassword.value){
-      passwordError.value ="Please make sure password is the same";
-      confirmPasswordError.value ="Please make sure password is the same";
+
+    if (password.value != confirmPassword.value) {
+      passwordError.value = "Please make sure password is the same";
+      confirmPasswordError.value = "Please make sure password is the same";
       success = false;
     }
-    if (!emailValid){
+
+    bool emailValid = RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email.value);
+    if (!emailValid) {
       emailError.value = "Please input a valid email";
       success = false;
     }
 
+    if (password.isEmpty) {
+      passwordError.value = "Please input a non-empty value";
+      success = false;
+    }
 
+    if (confirmPassword.isEmpty) {
+      confirmPasswordError.value = "Please input a non-empty value";
+      success = false;
+    }
 
-    if (!success ||!emailValid) {
+    if (password.value.length < 8 || password.value.length > 24) {
+      passwordError.value = "Password length must 8-24 characters";
+      success = false;
+    }
+
+    if (!success) {
       Get.dialog(const AlertDialog(
         title: Text("Sign Up Failed"),
         content: Text("Please check your credentials"),
       ));
+    } else {
+      Get.dialog(const AlertDialog(title: Text("Register Success")));
     }
-      if (success & emailValid) {
-        Get.dialog(const AlertDialog(
-          title: Text("Register Success")
-
-        ));
-
-    }
-
-
   }
-
 }
--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -6,107 +6,162 @@ import '../controllers/register_controller.dart';
 
 class RegisterView extends GetView<RegisterController> {
   const RegisterView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.blue.shade900,
+        //backgroundColor: Colors.blue.shade900,
         body: Container(
             width: double.infinity,
             padding: const EdgeInsets.all(32),
-
-            decoration: BoxDecoration(
-
-                borderRadius: BorderRadius.circular(32)),
-            child:Flex(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(32)),
+            child: Flex(
               direction: Axis.vertical,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  child: ListView(children: [
-                    Text('Welcome',style: Get.theme.textTheme.displaySmall),
-                    const SizedBox(height: 4,),
-                     Text('Hello there, Create an account',style: Get.theme.textTheme.bodyText1),
-                    const SizedBox(height: 26,),
+                  child: ListView(
+                    children: [
+                      Text('Welcome', style: Get.theme.textTheme.displaySmall),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Text('Hello there, Create an account',
+                          style: Get.theme.textTheme.bodyText1),
+                      const SizedBox(
+                        height: 26,
+                      ),
 
-                    const Text('Full name:',style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16),),
-                    const SizedBox(height: 4,),
+                      const Text(
+                        'Full name:',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
 
-                     Obx(
-                       ()=> TextField(
-                        onChanged:controller.onNameChange,
-                        decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Enter your full name",
-                        errorText: controller.fullNameError.value ==""?null:controller.fullNameError.value,
-                    ),),
-                     ),
-                    const SizedBox(height: 4,),
-                    const Text('Email:',style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16),),
-                    const SizedBox(height: 4,),
+                      Obx(
+                        () => TextField(
+                          onChanged: controller.onNameChange,
+                          decoration: InputDecoration(
+                            filled: true,
+                            hintText: "Enter your full name",
+                            errorText: controller.fullNameError.value == ""
+                                ? null
+                                : controller.fullNameError.value,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      const Text(
+                        'Email:',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
 
+                      Obx(
+                        () => TextField(
+                          onChanged: controller.onEmailChange,
+                          decoration: InputDecoration(
+                            filled: true,
+                            hintText: "Enter your email",
+                            errorText: controller.emailError.value == ""
+                                ? null
+                                : controller.emailError.value,
+                          ),
+                        ),
+                      ),
 
-                    Obx(
-                      ()=>  TextField(
-                        onChanged: controller.onEmailChange,
-                        decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Enter your email",
-                        errorText: controller.emailError.value ==""?null:controller.emailError.value,
-                      ),),
-                    ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      const Text(
+                        'Password:',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
 
-                    const SizedBox(height: 4,),
-                    const Text('Password:',style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16),),
-                    const SizedBox(height: 4,),
+                      Obx(
+                        () => TextField(
+                          onChanged: controller.onPasswordChange,
+                          obscureText: controller.showPassword.value,
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.remove_red_eye),
+                              onPressed: controller.toggleShowPassword,
+                            ),
+                            filled: true,
+                            hintText: "Enter your password",
+                            errorText: controller.passwordError.value == ""
+                                ? null
+                                : controller.passwordError.value,
+                          ),
+                        ),
+                      ),
 
-                    Obx(
-                      ()=> TextField(
-                        onChanged: controller.onPasswordChange,
-                        decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Enter your password",
-                        errorText: controller.passwordError.value ==""?null:controller.passwordError.value,
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      const Text(
+                        'Confirm Password:',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
 
-                      ),),
-                    ),
+                      Obx(
+                        () => TextField(
+                          onChanged: controller.onConfirmPasswordChange,
+                          obscureText: controller.showConfirmPassword.value,
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.remove_red_eye),
+                              onPressed: controller.toggleShowConfirmPassword,
+                            ),
+                            filled: true,
+                            hintText: "Confirm your password",
+                            errorText:
+                                controller.confirmPasswordError.value == ""
+                                    ? null
+                                    : controller.confirmPasswordError.value,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 26,
+                      ),
 
-                    const SizedBox(height: 4,),
-                    const Text('Confirm Password:',style: TextStyle(fontWeight: FontWeight.w700,fontSize: 16),),
-                    const SizedBox(height: 4,),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: controller.signUp,
+                          child: const Text("SIGN UP"),
+                        ),
+                      ),
 
-
-                    Obx(
-                      ()=> TextField(
-                        onChanged: controller.onConfirmPasswordChange,
-                        decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Confirm your password",
-                        errorText: controller.confirmPasswordError.value==""?null:controller.confirmPasswordError.value,
-
-                      ),),
-                    ),
-                    const SizedBox(height: 26,),
-
-
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: controller.signUp,
-                        child: const Text("SIGN UP"),),),
-
-                    //yong size box baoqilai ranhou infinty qu lakai
-
-                  ],),
+                      //yong size box baoqilai ranhou infinty qu lakai
+                    ],
+                  ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text("Already have an account ?"),
-                    TextButton(onPressed: (){}, child: const Text('Log In'))
-                  ],)
-
-              ],)
-        )
-    );
+                    TextButton(onPressed: () {}, child: const Text('Log In'))
+                  ],
+                )
+              ],
+            )));
   }
 }
